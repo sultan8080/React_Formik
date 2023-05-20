cf8ff006a7c20859c18e1fb1b86e658081b477ce
Moti-Sultan <motisultannur@gmail.com>
2023-05-20 11:37:26 +0200

    use of field in FORMIK

--- src/Newsletter.jsx
+++ src/Newsletter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const Newsletter = () => {
@@ -22,37 +22,34 @@ const Newsletter = () => {
         }, 400);
       }}
     >
-      {(formik) => (
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="firstName">First Name</label>
-          <input
-            id="firstName"
-            type="text"
-            {...formik.getFieldProps("firstName")}
-          />
-          {formik.touched.firstName && formik.errors.firstName ? (
-            <div>{formik.errors.firstName}</div>
-          ) : null}
-
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            id="lastName"
-            type="text"
-            {...formik.getFieldProps("lastName")}
-          />
-          {formik.touched.lastName && formik.errors.lastName ? (
-            <div>{formik.errors.lastName}</div>
-          ) : null}
-
-          <label htmlFor="email">Email Address</label>
-          <input id="email" type="email" {...formik.getFieldProps("email")} />
-          {formik.touched.email && formik.errors.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
-
-          <button type="submit">Submit</button>
-        </form>
-      )}
+      <Form className="mt-5 space-y-6 bg-slate-200">
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text" />
+        <ErrorMessage name="firstName" />
+        <br />
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text" />
+        <ErrorMessage name="lastName" />
+        <br />
+        <label htmlFor="email">Email Address</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
+        <br />
+        <input className="form-input" placeHolder="Jane" />
+        <Field name="firstName" className="form-input" placeholder="Jane" />
+        <br />
+        {/* <textarea className="form-textarea"/></textarea> */}
+        <Field name="message" as="textarea" className="form-textarea" />
+        {/* // <select className="my-select"/> */}
+        <br />
+        <Field name="colors" as="select" className="my-select">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+        </Field>
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
     </Formik>
   );
 };
